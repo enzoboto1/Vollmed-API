fc16a93804bd33c25e1335ebc6f2450cda461bda
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-04 17:14:10 -0300

    Adicionando MedicoService ao metodo cadastrarMedico para salvar o Medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import med.voll.api.dto.DadosCadastroMedicoDTO;
+import med.voll.api.service.MedicoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medico")
 public class MedicoController {
 
+    private final MedicoService medicoService;
+
+    public MedicoController(MedicoService medicoService) {
+        this.medicoService = medicoService;
+    }
+
     @PostMapping("/cadastrar")
-    public void cadastrarMedico(@RequestBody DadosCadastroMedicoDTO dados) {
-        System.out.println(dados);
+    public void cadastrarMedico(@RequestBody DadosCadastroMedicoDTO medicoDTO) {
+        medicoService.save(medicoDTO);
     }
 
 }
