a34667c14b2ce43a8c496c7baed6fabae48358b0
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-04 15:18:31 -0300

    Adicionando DadosEnderecoDTO

---
+++ src/main/java/med/voll/api/dto/DadosEnderecoDTO.java
@@ -0,0 +1,5 @@
+package med.voll.api.dto;
+
+public record DadosEnderecoDTO(String logradouro, String bairro, String cep, String cidade, String uf, String numero,
+                               String complemento) {
+}
