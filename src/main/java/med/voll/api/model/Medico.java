315f1ebaa946e61ab814c0714fc4beef331c6216
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-04 17:09:09 -0300

    Criando classe Medico

---
+++ src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,23 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private EspecialidadeMedico especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+}
