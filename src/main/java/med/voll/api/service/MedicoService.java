9ce9f2c59fbd466e067276093c0a3a2572d8715e
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-05 19:37:59 -0300

    Adicionando metodo para busca de todos medicos

--- src/main/java/med/voll/api/service/MedicoService.java
+++ src/main/java/med/voll/api/service/MedicoService.java
@@ -1,10 +1,14 @@
 package med.voll.api.service;
 
 import med.voll.api.dto.DadosCadastroMedicoDTO;
+import med.voll.api.dto.DadosListagemMedicoDTO;
 import med.voll.api.mapper.MedicoMapper;
 import med.voll.api.model.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MedicoService {
@@ -17,6 +21,12 @@ public class MedicoService {
 
     public Medico save(DadosCadastroMedicoDTO medicoDTO) {
         return medicoRepository.save(MedicoMapper.toMedico(medicoDTO));
+    }
+
+    public List<DadosListagemMedicoDTO> findAll(){
+        return medicoRepository.findAll().stream()
+                .map(MedicoMapper::toDadosListagemMedicoDTO)
+                .collect(Collectors.toList());
     }
 
 }
