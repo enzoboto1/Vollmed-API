0da64f7ff26fb52e8b1b4eb2da8d06486f617638
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-06 21:30:59 -0300

    Adicionando listar em  PacienteController

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,18 +2,34 @@ package med.voll.api.controller;
 
 import med.voll.api.dto.DadosCadastroMedicoDTO;
 import med.voll.api.dto.DadosCadastroPacienteDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.dto.DadosListagemMedicoDTO;
+import med.voll.api.dto.DadosListagemPacienteDTO;
+import med.voll.api.service.PacienteService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
 
+    private final PacienteService pacienteService;
+
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
     @PostMapping("/cadastrar")
     public void cadastrarPaciente(@RequestBody DadosCadastroPacienteDTO dados) {
-        System.out.println("dados recebido: " + dados);
+        pacienteService.save(dados);
+    }
+
+    @GetMapping
+    public List<DadosListagemPacienteDTO> listar() {
+        return pacienteService.findAll();
     }
 
 }
