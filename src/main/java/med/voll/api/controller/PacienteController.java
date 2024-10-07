0220dfbed389c9a22282f3042802715e62cf1eb9
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-06 22:17:14 -0300

    Adicionando listar em  PacienteController

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,16 +1,12 @@
 package med.voll.api.controller;
 
-import med.voll.api.dto.DadosCadastroMedicoDTO;
 import med.voll.api.dto.DadosCadastroPacienteDTO;
-import med.voll.api.dto.DadosListagemMedicoDTO;
 import med.voll.api.dto.DadosListagemPacienteDTO;
 import med.voll.api.service.PacienteService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/paciente")
@@ -28,8 +24,8 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPacienteDTO> listar() {
-        return pacienteService.findAll();
+    public Page<DadosListagemPacienteDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return pacienteService.findAll(paginacao);
     }
 
 }
