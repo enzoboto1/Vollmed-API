b7a0b53b382d6072303f4c7ea3bc9834ec4e7595
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-06 22:17:41 -0300

    Ajuste em DadosListagemMedicoDTO

--- src/main/java/med/voll/api/dto/DadosListagemMedicoDTO.java
+++ src/main/java/med/voll/api/dto/DadosListagemMedicoDTO.java
@@ -1,6 +1,13 @@
 package med.voll.api.dto;
 
 import med.voll.api.model.EspecialidadeMedico;
+import med.voll.api.model.Medico;
 
-public record DadosListagemMedicoDTO(String nome, String email, String crm, EspecialidadeMedico especialidade) {
+public record DadosListagemMedicoDTO(Long id, String nome, String email, String crm,
+                                     EspecialidadeMedico especialidade) {
+
+    public DadosListagemMedicoDTO(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+
 }
