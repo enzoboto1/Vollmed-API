67944c3f09d56bca9d6e00d343ef79803fc885f0
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-06 21:30:08 -0300

    Adicionando nova classe DadosCadastroPacienteDTO

--- src/main/java/med/voll/api/dto/DadosCadastroPacienteDTO.java
+++ src/main/java/med/voll/api/dto/DadosCadastroPacienteDTO.java
@@ -1,5 +1,16 @@
 package med.voll.api.dto;
 
-public record DadosCadastroPacienteDTO(String nome, String email, String telefone, String cpf,
-                                       DadosEnderecoDTO endereco) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosCadastroPacienteDTO(
+        @NotBlank String nome,
+        @NotBlank @Email String email,
+        @NotBlank String telefone,
+        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
+        @NotNull @Valid DadosEnderecoDTO endereco
+) {
 }
