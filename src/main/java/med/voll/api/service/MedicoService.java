86f3a929841cd868259b6386d4260ef091b34676
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-06 23:44:06 -0300

    Adicionando metodo update

--- src/main/java/med/voll/api/service/MedicoService.java
+++ src/main/java/med/voll/api/service/MedicoService.java
@@ -1,7 +1,10 @@
 package med.voll.api.service;
 
+import med.voll.api.dto.DadosAtualizacaoMedicoDTO;
 import med.voll.api.dto.DadosCadastroMedicoDTO;
+import med.voll.api.dto.DadosEnderecoDTO;
 import med.voll.api.dto.DadosListagemMedicoDTO;
+import med.voll.api.mapper.EnderecoMapper;
 import med.voll.api.mapper.MedicoMapper;
 import med.voll.api.model.Medico;
 import med.voll.api.repository.MedicoRepository;
@@ -25,6 +28,53 @@ public class MedicoService {
     public Page<DadosListagemMedicoDTO> findAll(Pageable paginacao) {
         return medicoRepository.findAll(paginacao)
                 .map(MedicoMapper::toDadosListagemMedicoDTO);
+    }
+
+    public void update(DadosAtualizacaoMedicoDTO dados) {
+        Medico medico = medicoRepository.getReferenceById(dados.id());
+        verificaDadosMedicoDTO(dados, medico);
+        verificaDadosEnderecoDTO(dados, medico);
+        medicoRepository.save(medico);
+    }
+
+    private void verificaDadosMedicoDTO(DadosAtualizacaoMedicoDTO dados, Medico medico) {
+        if (dados.nome() != null) {
+            medico.setNome(dados.nome());
+        }
+        if (dados.telefone() != null) {
+            medico.setTelefone(dados.telefone());
+        }
+        if (dados.endereco() != null) {
+            medico.setEndereco(EnderecoMapper.toEndereco(dados.endereco()));
+        }
+    }
+
+    private void verificaDadosEnderecoDTO(DadosAtualizacaoMedicoDTO dados, Medico medico) {
+        if (dados.endereco() != null) {
+            DadosEnderecoDTO endereco = dados.endereco();
+
+            if (endereco.logradouro() != null) {
+                medico.getEndereco().setLogradouro(endereco.logradouro());
+            }
+            if (endereco.bairro() != null) {
+                medico.getEndereco().setBairro(endereco.bairro());
+            }
+            if (endereco.cep() != null) {
+                medico.getEndereco().setCep(endereco.cep());
+            }
+            if (endereco.uf() != null) {
+                medico.getEndereco().setUf(endereco.uf());
+            }
+            if (endereco.cidade() != null) {
+                medico.getEndereco().setCidade(endereco.cidade());
+            }
+            if (endereco.numero() != null) {
+                medico.getEndereco().setNumero(endereco.numero());
+            }
+            if (endereco.complemento() != null) {
+                medico.getEndereco().setComplemento(endereco.complemento());
+            }
+        }
     }
 
 }
