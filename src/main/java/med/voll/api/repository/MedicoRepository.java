4df6a68fc180c5d0ed74765619efabb163ee28f1
enzoboto1 <enzoboto1.0@gmail.com>
2024-10-21 23:41:29 -0300

    Adicionando metodo findAllByAtivoTrue com Pageable

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -1,7 +1,14 @@
 package med.voll.api.repository;
 
 import med.voll.api.model.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+    Page<Medico> findAllByAtivoTrue(Pageable pageable);
+
 }
